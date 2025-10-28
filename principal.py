11e2140a5bdf1149afd6e2bcb7e7b89a6d6ebdca
Artur Velho <avelhoaa@gmail.com>
2025-10-28 15:40:04 -0300

    Add files via upload

--- principal.py
+++ principal.py
@@ -1,5 +1,7 @@
 import tkinter as tk
+import tkinter
 import mido
+import sqlite3
 
 # Variaveis Globais
 global gbg, gbgbt, gbgmt
@@ -12,7 +14,11 @@ gbgbtplay="aquamarine2"
 gbgbtpause="aquamarine2"
 
 
+<<<<<<< /tmp/tsoUyM_principal.py
 
+=======
+# Funcao para listar Dispositivos MIDI
+>>>>>>> principal.py
 def listar_dispositivos_midi():
     """
     Retorna uma lista dos nomes dos dispositivos MIDI de entrada.
@@ -23,12 +29,76 @@ def listar_dispositivos_midi():
     except Exception as e:
         return [f"Erro ao listar dispositivos: {e}"]
 
+#def
+
+
+
+# Funcao Criar Menu Principal
 def criar_gui():
+    # Cria Janela
     root = tk.Tk()
+<<<<<<< /tmp/tsoUyM_principal.py
     root.geometry("800x600")
     root.title("Controlador KFX CR9")
     root.bind("<Escape>", quit)
     root.config(bg=gbg)
+=======
+    root.geometry("800x600") # Tamanho Janela
+    root.title("Controlador KFX CR9") # Titulo Janela
+    root.bind("<Escape>", quit) # Comportamento ao Teclar ESC
+    root.config(bg=gbg) # Configurações fundo
+
+
+    #Conecta Banco de Dados Configurações
+    conn = sqlite3.connect('./conf/menu.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM menu order by col_menu, pos_menu")
+    rows = cursor.fetchall()
+
+
+    # Cria Menu Principal
+    menubar = tk.Menu(root)
+    menubar.config(bg=gbgbt)
+    root.config(menu=menubar)
+
+
+    # Criar Lista Menu Secundarios
+    #cursor.execute("SELECT * FROM menu where pos_menu != 0 order by col_menu, pos_menu")
+    #rows = cursor.fetchall()
+    #print(rows)
+
+    #for imen in rows:
+    #    i = 0
+    #   file_menu = tk.Menu(menubar, tearoff=0) # tearoff=0 remove a linha pontilhada que permite "desligar" o menu
+    #    file_menu.add_command(label=imen, menu=file_menu)
+
+    #file_menu = tk.Menu(menubar, tearoff=0) # tearoff=0 remove a linha pontilhada que permite "desligar" o menu
+
+    # Cria Lista Menu BaseException
+    for row in rows:
+        if row[3] == 0:
+            mp = f'{row[4]} = tk.Menu(menubar, tearoff=0)' # tearoff=0 remove a linha pontilhada que permite "desligar" o menu
+            print(mp)
+            exec(mp)
+            #print(f'{row[4]} = tk.Menu(menubar, tearoff=0)')
+            x = f'menubar.add_cascade(label="{row[0]}", menu={row[4]})'
+            exec(x)
+            print(x)
+        #    print(row[4])
+        else:
+            mm = f'{row[4]}.add_command(label="{row[0]}", command=row[1])'
+            exec(mm)
+            print(mm)
+            #print(row)
+            #print(f'{row[4]}.add_command(label="Texto")')
+            #f'{row[4]}.add_command(label="Texto")'
+
+
+    root.config(menu=menubar)
+
+
+
+>>>>>>> principal.py
     
     # Listbox
     listbox = tk.Listbox(root, width=35, height=2, bg=gbgbt, highlightbackground=gbgbt)
@@ -77,4 +147,8 @@ def criar_gui():
 
 
 if __name__ == "__main__":
+<<<<<<< /tmp/tsoUyM_principal.py
     criar_gui()
+=======
+    criar_gui()
+>>>>>>> principal.py
